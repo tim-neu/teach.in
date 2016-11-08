2706fc9124fba9a752e0da06143b38c21921913b
jesse <jesseaborden@gmail.com>
2016-11-08 09:08:02 -0800

    fixed past merge conflicts

--- client/app.js
+++ client/app.js
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor (props) {
-<<<<<<< HEAD
     super(props),
     this.state = {};
   };
-
-=======
-    super(props)
-    
-    this.state = {};
-  };
->>>>>>> shared-components
   render () {
     return (
       <div>
