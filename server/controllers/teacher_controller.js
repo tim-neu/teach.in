91066407ae5c8a894bc080f9980d10e3c3df5a9c
John Huynh <huynh.j93@gmail.com>
2016-11-08 18:21:21 -0800

    [add]-Added Seeding

--- server/controllers/teacher_controller.js
+++ server/controllers/teacher_controller.js
@@ -1,18 +1,37 @@
 var teacherController = {};
 const Teacher = require('../models/teacher_model');
+const Class = require ('../models/class_model');
 teacherController.SIGNUP = (req, res) => {
 
+
+	//seeding two classes automatically for this teacher
+	var classArr = [];
+	for (var i = 0; i < 2; i++) {
+		var classI = Class.build({
+			name: 'class ' + i,
+		})
+		classArr.push(classI);
+	}
 	console.log('i changed the password from 123 to:', req.body.password);
 	Teacher.create({
 		name: req.body.name,
 		email: req.body.email,
 		password: req.body.password,
 	}).then((teacher) => {
+
+		//Code below seeds two classes to the teacher;
+		for (var i = 0; i < classArr.length; i++) {
+			classArr[i].save().then(function () {
+				this.setTeacher(teacher);
+			});
+		};
+	
 		return teacher;
 	})
 	.catch((err) => {
 		console.log('err in creating teacher signup:', err);
 	});
+
 	res.send(req.body);
 };
 
