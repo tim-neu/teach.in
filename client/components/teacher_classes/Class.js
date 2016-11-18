922a9b186adfe0394258fedbdabc80f6dfc0e311
John Huynh <huynh.j93@gmail.com>
2016-11-17 19:41:12 -0800

    [add]-Styling for grades

--- client/components/teacher_classes/Class.js
+++ client/components/teacher_classes/Class.js
@@ -160,7 +160,7 @@ class Class extends Component {
 							<AssignmentList classId={this.state.classId} classTitle={this.state.className}/>
   				  </div>
   				  <div className="col-lg-8">
-  				  	<h4>Click to edit form goes here</h4>
+  				  	<h4>Grades</h4>
 				  	<AssignmentGradesForm currentAssignment={this.props.currentAssignment} associated={this.props.currentAssociatedStudents} students={this.props.currentAssociatedStudents} classTitle={this.state.className} onSubmit={this.onSubmit}/>
   				  </div>
     	  	</div>
